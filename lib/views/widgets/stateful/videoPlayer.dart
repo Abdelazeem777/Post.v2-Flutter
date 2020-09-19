fa5214c681cc858ba45062735852408dbc8ba076
Abdelazeem Kuratem <abdelazeem263@gmail.com>
2020-09-20 01:12:11 +0200

    add video player for video-type posts

---
+++ lib/views/widgets/stateful/videoPlayer.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:post/style/appColors.dart';
+import 'package:post/utils/sizeConfig.dart';
+import 'package:video_player/video_player.dart';
+
+class CustomVideoPlayer extends StatefulWidget {
+  String videoURL;
+  CustomVideoPlayer({this.videoURL});
+
+  @override
+  _CustomVideoPlayerState createState() => _CustomVideoPlayerState();
+}
+
+class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(widget.videoURL)
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      width: SizeConfig.screenWidth,
+      height: SizeConfig.screenWidth,
+      child: Stack(
+        children: [
+          Center(
+            child: _controller.value.initialized
+                ? AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
+                  )
+                : Container(),
+          ),
+          Positioned(
+            top: 16,
+            right: 16,
+            child: IconButton(
+                icon: Icon(
+                  _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                  color: AppColors.PRIMARY_COLOR,
+                  size: 30,
+                ),
+                onPressed: () => setState(() {
+                      _controller.value.isPlaying
+                          ? _controller.pause()
+                          : _controller.play();
+                    })),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
