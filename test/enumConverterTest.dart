4786042bd2e9df774f97c61687e365ef240499aa
Abdelazeem Kuratem <abdelazeem263@gmail.com>
2020-10-01 04:16:43 +0200

    add enum converter  to string test cases

---
+++ test/enumConverterTest.dart
@@ -0,0 +1,18 @@
+import 'package:enum_to_string/enum_to_string.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:post/enums/reactTypeEnum.dart';
+
+void main() {
+  test("convert enum to string", () {
+    var result = EnumToString.convertToString(ReactType.Love);
+    expect(result, "Love");
+  });
+  test('convert string to enum', () {
+    var result = EnumToString.fromString(ReactType.values, 'Love');
+    expect(result, ReactType.Love);
+  });
+  test('convert wrong string to enum', () {
+    var result = EnumToString.fromString(ReactType.values, 'ReactType.Love');
+    expect(result, null);
+  });
+}
