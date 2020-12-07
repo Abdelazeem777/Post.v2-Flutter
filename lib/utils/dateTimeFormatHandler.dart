b09fab4cd97e5fc640314cf403350740e00a1969
Abdelazeem Kuratem <abdelazeem263@gmail.com>
2020-12-07 04:16:23 +0200

    enhance DateTimeFormatHandler class to adapt with server delation

--- lib/utils/dateTimeFormatHandler.dart
+++ lib/utils/dateTimeFormatHandler.dart
@@ -6,6 +6,7 @@ class DateTimeFormatHandler {
 
   static String getDurationFromTimestamp(int timestamp) {
     DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(timestamp);
+    now = DateTime.now();
     return getDurationFromDateTime(dateTime);
   }
 
@@ -16,51 +17,53 @@ class DateTimeFormatHandler {
     else if (_isNow(dateTime))
       return 'now';
     else if (_isLessThan1min(dateTime)) {
-      int difference = now.difference(dateTime).inSeconds;
+      int difference = DateTime.now().difference(dateTime).inSeconds;
       return '${difference}s';
     } else if (_isLessThan1hour(dateTime)) {
-      int difference = now.difference(dateTime).inMinutes;
+      int difference = DateTime.now().difference(dateTime).inMinutes;
       return '${difference}m';
     } else if (_isLessThan24hours(dateTime)) {
-      int difference = now.difference(dateTime).inHours;
+      int difference = DateTime.now().difference(dateTime).inHours;
       return '${difference}h';
     } else if (_isLessThan7Days(dateTime)) {
-      int difference = now.difference(dateTime).inDays;
+      int difference = DateTime.now().difference(dateTime).inDays;
       return '${difference}d';
     } else {
-      return (dateTime.year == now.year)
+      return (dateTime.year == DateTime.now().year)
           ? '${getMonthName(dateTime.month)} ${dateTime.day}'
           : '${dateTime.year} ${getMonthName(dateTime.month)} ${dateTime.day}';
     }
   }
 
   static bool _isNow(DateTime dateTime) {
-    DateTime earlier1Sec = now.subtract(Duration(seconds: 50));
-    return dateTime.isAfter(earlier1Sec) && dateTime.isBefore(now);
+    DateTime earlier50sec = DateTime.now().subtract(Duration(seconds: 50));
+    var forServerDelayError = Duration(seconds: 20);
+    var timeNow = DateTime.now().add(forServerDelayError);
+    return dateTime.isAfter(earlier50sec) && dateTime.isBefore(timeNow);
   }
 
   static bool _isLessThan1min(DateTime dateTime) {
-    DateTime earlier1Min = now.subtract(Duration(minutes: 1));
+    DateTime earlier1Min = DateTime.now().subtract(Duration(minutes: 1));
     return dateTime.isAfter(earlier1Min);
   }
 
   static bool _isLessThan1hour(DateTime dateTime) {
-    DateTime earlier1hour = now.subtract(Duration(hours: 1));
+    DateTime earlier1hour = DateTime.now().subtract(Duration(hours: 1));
     return dateTime.isAfter(earlier1hour);
   }
 
   static bool _isLessThan24hours(DateTime dateTime) {
-    DateTime earlier24hours = now.subtract(Duration(hours: 24));
+    DateTime earlier24hours = DateTime.now().subtract(Duration(hours: 24));
     return dateTime.isAfter(earlier24hours);
   }
 
   static bool _isLessThan7Days(DateTime dateTime) {
-    DateTime earlier7Days = now.subtract(Duration(days: 7));
+    DateTime earlier7Days = DateTime.now().subtract(Duration(days: 7));
     return dateTime.isAfter(earlier7Days);
   }
 
   static bool _isDateTimeInTheFuture(DateTime dateTime) =>
-      dateTime.isAfter(now);
+      dateTime.isAfter(DateTime.now().add(Duration(seconds: 15)));
 }
 
 String getMonthName(int month) {
